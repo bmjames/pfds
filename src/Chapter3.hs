92db959815536ed9fe1b5c50a00a5a3a3b03b21b
Ben James <ben.james@guardian.co.uk>
2014-12-28 13:35:35 +0000

    Refactor merge using rank function

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -10,6 +10,10 @@ type Rank = Int
 data Heap a = Empty | Node Rank a (Heap a) (Heap a)
             deriving Show
 
+rank :: Heap a -> Rank
+rank Empty = 0
+rank (Node r _ _ _) = r
+
 unit :: a -> Heap a
 unit x = Node 1 x Empty Empty
 
@@ -23,10 +27,8 @@ instance HeapLike Heap where
     merge n1@(Node _ a1 _ _) n2@(Node _ a2 _ _) | a2 > a1   = merge' n1 n2
                                                 | otherwise = merge' n2 n1
       where
-        merge' (Node s1 a1 l1 r1) n2@(Node s2 a2 l2 r2) = -- a2 is greater
-            case l1 of Empty -> Node s1 a1 (merge n2 r1) Empty
-                       Node s3 a3 l3 r3 | s3 < s1   -> Node (succ s3) a1 (merge n2 r1) l1
-                                        | otherwise -> Node (succ s2) a1 l1 (merge n2 r1)
+        merge' (Node s1 a l1 r1) n | rank l1 < s1 = Node (succ (rank l1)) a (merge n r1) l1
+                                   | otherwise    = Node (succ (rank n)) a l1 (merge n r1)
 
 main :: IO ()
 main = print (foldr insert empty ['y', 'x', 'a', 'f', 'b', 'h', 'c'] :: Heap Char)
